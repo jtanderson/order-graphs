44f1329e8f0285b0f21c122f6feebde63ca1b566
Joe Anderson <jtanderson@salisbury.edu>
2019-07-07 20:21:44 -0400

    some prototyping

---
+++ README.md
@@ -0,0 +1,7 @@
+# Order Graphs
+===
+
+The primary goal of this code is to experimentally investigate some conjectures
+regarding the structure of particular Hasse diagrams.
+A secondary goal is to learn and familiarize the author with the D programming
+language and its toolchain.
--- main.d
+++ main.d
@@ -1,14 +1,76 @@
 /*
-* File: main.d
-*
-* Experimental file to attempt brute-forcing the construction of partial order
-* graphs Gamma(k,n) and then to investigate their structural properties.
-*
-* @author: Joe Anderson <jtanderson@salisbury.edu>
-* @date:   3 July 2019
-*/
+ * File: main.d
+ *
+ * Experimental file to attempt brute-forcing the construction of partial order
+ * graphs Gamma(k,n) and then to investigate their structural properties.
+ *
+ * @author: Joe Anderson <jtanderson@salisbury.edu>
+ * @date:   3 July 2019
+ */
 
 import std.stdio : writeln, writefln;
+
+/*
+   A tuple of integers capturing the structure of a young diagram
+ */
+alias ydiagram = int[];
+
+struct Graph {
+  struct node {
+    ydiagram yd;
+    node* down = null;
+    node* up = null;
+  }
+
+  int n, k;
+
+  // record which nodes are at which level in the graph
+  node*[][] nodes;
+
+  node* root;
+
+  this(int k, int n){
+    this.n = n;
+    this.k = k;
+
+    root = new node;
+    node* cursor = root;
+
+    nodes.length = this.n * (this.n - this.k);
+
+    // append the root to layer 0
+    nodes[0] ~= root;
+    int level = 1;
+
+    // connect-four logic: can choose any row that is < the one above it
+    // note: this corresponds to incrementing a coordinate in the corresponding
+    // tuple representation of the diagram
+    while( !isFull(cursor) ){
+      auto tmp = new node;
+      tmp.yd = cursor.yd.dup;
+    }
+
+    // make sure the last node in the last level (there should only be one) is full
+    assert(nodes[$].length == 1);
+    assert(isFull(nodes[$][$]));
+  }
+
+  // all rows are used
+  bool isRowFull(node* n){
+    return n.yd[$] > 0;
+  }
+
+  // all cols are used
+  bool isColFull(node* n){
+    return n.yd[0] > 0; 
+  }
+
+  // all spaces are full
+  bool isFull(node* n){
+    return n.yd[$] == this.n - this.k;
+  }
+}
+
 
 
 int main(){
